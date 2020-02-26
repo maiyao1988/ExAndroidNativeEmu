0b4868e4fed6029036a7026a5adda1520f320dde
yao.mai <yao.mai@dingxiang-inc.com>
2020-02-26 11:01:48 +0800

    missing file

---
+++ androidemu/java/classes/clazz.py
@@ -0,0 +1,15 @@
+from androidemu.java.java_class_def import JavaClassDef
+from androidemu.java.java_field_def import JavaFieldDef
+from androidemu.java.java_method_def import java_method_def, JavaMethodDef
+
+class Class(metaclass=JavaClassDef, jvm_name='java/lang/Class'):
+    class_loader = None
+    def __init__(self, jvm_name):
+        self.__jvm_name = jvm_name
+    #
+
+    @java_method_def(name='getClassLoader', signature='()Ljava/lang/ClassLoader;', native=False)
+    def getClassLoader(self, emu):
+        return Class.class_loader
+    #
+#
