34e1b9d1b52c610451fb7ab9b096a98892607d43
yao.mai <yao.mai@dingxiang-inc.com>
2020-03-20 23:23:16 +0800

    重构，标准化java类型输入和输出

---
+++ androidemu/java/classes/array.py
@@ -0,0 +1,34 @@
+from androidemu.java.java_class_def import JavaClassDef
+from androidemu.java.java_field_def import JavaFieldDef
+from androidemu.java.java_method_def import java_method_def, JavaMethodDef
+
+class Array(metaclass=JavaClassDef, jvm_name='java/lang/reflect/Array'):
+
+    def __init__(self, jvm_type, pyitems):
+        self.__pyitems = pyitems
+        self.__jvm_type = jvm_type
+    #
+
+    def get_py_items(self):
+        return self.__pyitems
+    #
+
+    def __len__(self):
+        return len(self.__pyitems)
+    #
+
+    def __getitem__(self,index):
+        return self.__pyitems[index]
+    #
+
+    def __setitem__(self,index,value):
+        self.__pyitems[index] = value
+    #
+
+    @staticmethod
+    @java_method_def(name='set', signature='(Ljava/lang/Object;I)Ljava/lang/Object;', native=False)
+    def set(emu, obj, index):
+        raise NotImplementedError()
+    #
+    
+#
