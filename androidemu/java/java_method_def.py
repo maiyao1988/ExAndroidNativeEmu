bd40806517997c7781d7598f7a57a824c24f83a8
zjzdy <zjzengdongyang@163.com>
2021-03-20 19:19:49 +0800

    处理JNI调用时的负数、浮点数、8字节数参数

--- androidemu/java/java_method_def.py
+++ androidemu/java/java_method_def.py
@@ -15,7 +15,7 @@ class JavaMethodDef:
         self.modifier = modifier
         self.ignore = ignore
 
-
+num_type_len = {"jlong": 8, "jint": 4, "jshort": 2, "jbyte": 1}
 def java_method_def(name, signature, native=False, args_list=None, modifier=None, ignore=False):
     def java_method_def_real(func):
         def native_wrapper(*args, **kwargs):
@@ -35,12 +35,39 @@ def java_method_def(name, signature, native=False, args_list=None, modifier=None
                 extra_args = args[1:]
             #
 
+            # 转换基本类型为对应的int数值，处理负数、浮点数、8字节数
+            new_extra_args = []
+            if args_list and len(args_list) == len(extra_args):
+                for i in range(len(extra_args)):
+                    if isinstance(extra_args[i], int) and extra_args[i] < 0 and args_list[i] in ("jint", "jshort", "jbyte"):
+                        b = extra_args[i].to_bytes(num_type_len[args_list[i]], byteorder='little', signed=True)
+                        new_extra_args.append(int.from_bytes(b, byteorder='little', signed=False))
+                    elif isinstance(extra_args[i], float) and args_list[i] == "jfloat":
+                        b = struct.pack("<f", extra_args[i])
+                        new_extra_args.append(int.from_bytes(b, byteorder='little', signed=False))
+                    elif isinstance(extra_args[i], int) and args_list[i] == "jlong":
+                        d = extra_args[i]
+                        if extra_args[i] < 0:
+                            b = extra_args[i].to_bytes(8, byteorder='little', signed=True)
+                            d = int.from_bytes(b, byteorder='little', signed=False)
+                        new_extra_args.append(d & 0xFFFFFFFF)
+                        new_extra_args.append((d >> 32) & 0xFFFFFFFF)
+                    elif isinstance(extra_args[i], float) and args_list[i] == "jdouble":
+                        b = struct.pack("<d", extra_args[i])
+                        d = int.from_bytes(b, byteorder='little', signed=False)
+                        new_extra_args.append(d & 0xFFFFFFFF)
+                        new_extra_args.append((d >> 32) & 0xFFFFFFFF)
+                    else:
+                        new_extra_args.append(extra_args[i])
+            else:
+                new_extra_args = extra_args
+
             return emulator.call_native(
                 native_wrapper.jvm_method.native_addr,
                 emulator.java_vm.jni_env.address_ptr,  # JNIEnv*
                 thiz,    # this, TODO: Implement proper "this", a reference to the Java object inside which this native
                          # method has been declared in
-                *extra_args  # Extra args.
+                *new_extra_args  # Extra args.
             )
         #
         def normal_wrapper(*args, **kwargs):
